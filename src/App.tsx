9633ef324e061b06b719ec16fb5c4e42d392f957
PavelM <myagkiypavel@gmail.com>
2023-09-18 18:41:47 +0300

    chore: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { CircularProgress } from "./components/Card/CircularProgress/CircularProgress";
+import { HoverEffects } from "./components/Card/HoverEffects/HoverEffects";
+import { RotatingImage } from "./components/Card/RotatingImage/RotatingImage";
 import { FlipCard } from "./components/Flexbox/FlipCard/FlipCard";
 import { FullscreenHoverEfects } from "./components/Flexbox/FullscreenHoverEfects/FullscreenHoverEfects";
-import { HoverEffects } from "./components/Flexbox/HoverEffects/HoverEffects";
 import { ResponsiveMansory } from "./components/Flexbox/ResponsiveMansory/ResponsiveMansory";
 import { SplitScreen } from "./components/Flexbox/SplitScreen/SplitScreen";
 import { SplitScreen2 } from "./components/Flexbox/SplitScreen2/SplitScreen2";
@@ -10,12 +12,19 @@ import styles from "./style.module.scss";
 function App() {
   return (
     <div className={styles.body}>
+      {/* Card */}
       {/* <HoverEffects /> */}
-      {/* <FullscreenHoverEfects /> */}
-      {/* <ResponsiveMansory /> */}
-      {/* <SplitScreen />} */}
-      {/* <SplitScreen2 /> */}
-      <FlipCard />
+      {/* <CircularProgress /> */}
+      <RotatingImage />
+
+      {/* Flexbox */}
+
+      {/* <HoverEffects />
+      <FullscreenHoverEfects />
+      <ResponsiveMansory />
+      <SplitScreen />
+      <SplitScreen2 />
+      <FlipCard /> */}
     </div>
   );
 }
