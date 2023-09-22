389e3388b0f5952e2ccde94d310da7eb6d5df0db
PavelM <myagkiypavel@gmail.com>
2023-09-22 19:56:36 +0300

    chore: NoteFound

--- src/components/ResponsiveWebsite/NotFoundPage/NotFoundPage.tsx
+++ src/components/ResponsiveWebsite/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,26 @@
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
+import planeImage from "./img/plane.png";
 
 const NotFoundPage = () => {
   return (
     <div className={styles.container}>
-      <h2>404</h2>
-      <p>Opps...looks like you got lost</p>
-      <button>Go Home</button>
-    </div>
-  )
-}
+      <div className={styles.ceiling}>
+        <div className={styles.imgBox}>
+          <img src={planeImage} alt="plane" />
+        </div>
+        <div>
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+      </div>
 
-export default NotFoundPage
+      <div className={styles.floor}>
+        <p>Opps...looks like you got lost</p>
+        <button>Go Home</button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
