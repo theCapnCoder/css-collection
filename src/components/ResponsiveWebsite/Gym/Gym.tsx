cdfc45e3dfd90bb9f58f7a3a09a22e364e5119ce
PavelM <myagkiypavel@gmail.com>
2023-09-23 18:44:56 +0300

    feat: Gym template

---
+++ src/components/ResponsiveWebsite/Gym/Gym.tsx
@@ -0,0 +1,32 @@
+import styles from "./styles.module.scss";
+import video from "./video/box.mp4";
+
+const Gym = () => {
+  return (
+    <div className={styles.gym}>
+      <div className={styles.logo}>
+        <span>Logo</span>
+      </div>
+
+      <div className={styles.content}>
+        <h2>Be stronger</h2>
+        <h3>Never give up</h3>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          facilis reiciendis quas necessitatibus perferendis, minus doloremque
+          eligendi veniam quis? Illo, officia quia rerum minima odit, quas minus
+          harum ipsam eveniet laudantium temporibus itaque quibusdam tenetur,
+          quisquam neque. Ullam inventore ab dolorem dolore dolorum porro
+        </p>
+        <button>Join Us</button>
+      </div>
+      <div className={styles.videoBox}>
+        {/* <video src={video} muted loop autoPlay></video> */}
+        <video src={video}></video>
+      </div>
+    </div>
+  );
+};
+
+export default Gym;
