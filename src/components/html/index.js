35b7acad09e4aa6fcad8169883e8bf82366e4041
PavelM <myagkiypavel@gmail.com>
2023-09-24 15:20:22 +0300

    async: part proggramm

--- src/components/html/index.js
+++
@@ -1,4 +0,0 @@
-console.log("test");
-const url = "https://api.escuelajs.co/api/v1/products";
-const data = fetch(url)
-console.log({ data });
---
+++ src/components/html/script.js
@@ -0,0 +1,17 @@
+console.log("test");
+const url = "https://api.escuelajs.co/api/v1/products";
+const data = fetch(url)
+console.log({ data });
+
+function now() {
+  return 21
+}
+
+function later() {
+  answer = answer *2 
+  console.log("Meaning of life", answer)
+}
+
+var answer = now()
+
+setTimeout(later, 1000)
