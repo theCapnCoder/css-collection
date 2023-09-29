6297fdc7b3ffca5bdbacfb62860dca16fe43a575
PavelM <myagkiypavel@gmail.com>
2023-09-29 12:33:14 +0300

    feat: CoolHoverEffect

--- src/components/Flexbox/CoolHoverEffects/CoolHoverEffects.tsx
+++ src/components/Flexbox/CoolHoverEffects/CoolHoverEffects.tsx
@@ -2,14 +2,17 @@ import styles from "./styles.module.scss";
 
 const cards = [
   {
+    number: "01",
     title: "Service One",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem expedita sequi quam error sunt! In.",
   },
   {
+    number: "02",
     title: "Service Two",
     text: "Lorem ipsum dolor sit amet text one new and what consectetur adipisicing elit. Dolorem expedita sequi quam error sunt! In.",
   },
   {
+    number: "03",
     title: "Service Three",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem expedita sequi quam error sunt! In.",
   },
@@ -20,7 +23,8 @@ const CoolHoverEffects = () => {
     <div className={styles.wrap}>
       <div className={styles.container}>
         {cards.map((card, index) => (
-          <div key={index} className={styles.card}>
+          <div key={index} className={styles.box}>
+            <h2>{card.number}</h2>
             <h3>{card.title}</h3>
             <p>{card.text}</p>
           </div>
