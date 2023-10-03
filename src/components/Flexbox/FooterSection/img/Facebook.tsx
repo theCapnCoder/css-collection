7374ac717c871968fd0e198f513f2ea3d4b18c28
PavelM <myagkiypavel@gmail.com>
2023-10-03 18:32:22 +0300

    feat: add img FooterSection

---
+++ src/components/Flexbox/FooterSection/img/Facebook.tsx
@@ -0,0 +1,17 @@
+const Facebook = () => {
+  return (
+    <svg fill="#000000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M17 1H3c-1.1 0-2 .9-2 2v14c0 1.101.9 2 2 2h7v-7H8V9.525h2v-2.05c0-2.164 1.212-3.684 3.766-3.684l1.803.002v2.605h-1.197c-.994 0-1.372.746-1.372 1.438v1.69h2.568L15 12h-2v7h4c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"></path>
+      </g>
+    </svg>
+  );
+};
+
+export default Facebook;
---
+++ src/components/Flexbox/FooterSection/img/YouTube.tsx
@@ -0,0 +1,30 @@
+const YouTube = () => {
+  return (
+    <svg
+      fill="#000000"
+      height="200px"
+      width="200px"
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="-143 145 512 512"
+      xmlSpace="preserve"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <polygon points="78.9,450.3 162.7,401.1 78.9,351.9 "></polygon>{" "}
+          <path d="M-143,145v512h512V145H-143z M241,446.8L241,446.8c0,44.1-44.1,44.1-44.1,44.1H29.1c-44.1,0-44.1-44.1-44.1-44.1v-91.5 c0-44.1,44.1-44.1,44.1-44.1h167.8c44.1,0,44.1,44.1,44.1,44.1V446.8z"></path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export default YouTube;
