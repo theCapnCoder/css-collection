0d364f9810fb8cefe826b09d761790efdb331292
PavelM <myagkiypavel@gmail.com>
2023-10-04 20:10:48 +0300

    chore: FooterSection

--- src/components/Flexbox/FooterSection/FooterSection.tsx
+++ src/components/Flexbox/FooterSection/FooterSection.tsx
@@ -8,57 +8,63 @@ import Instagram from "./img/Instagram";
 const FooterSection = () => {
   return (
     <div className={styles.wrap}>
-      <footer className={styles.container}>
-        <div className={clsx(styles.sec, styles.aboutUs)}>
-          <h2>About Us</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ea
-            quisquam sit eveniet, deleniti consectetur in soluta accusantium
-            eligendi et consequatur ut aliquid, officia quas ab dolore excepturi
-            pariatur quia numquam accusamus. Consequuntur voluptate illo quasi
-            Dignissimos distinctio repellat repudiandae dolorem obcaecati.
-            laborum, quidem dolorum voluptates provident debitis ullam iste fuga
-            minus voluptatum vero.
-          </p>
-          <ul className={styles.socials}>
-            <li>
-              <a href="#">
-                <Facebook />
-              </a>
-              <a href="#">
-                <Twitter />
-              </a>
-              <a href="#">
-                <Instagram />
-              </a>
-              <a href="#">
-                <YouTube />
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div>
-          <h3>Quck Links</h3>
-          <p>About</p>
-          <p>FAQ</p>
-          <p>Privacy Policy</p>
-          <p>Help</p>
-          <p>Terms & Conditions</p>
-          <p>Contact</p>
-        </div>
-
-        <div>
-          <h3>Contact Us</h3>
-          <div>
-            <p>647 Linda Street Phoenixville, PA 163460, USA</p>
+      <footer>
+        <div className={styles.container}>
+          <div className={clsx(styles.sec, styles.aboutUs)}>
+            <h2>About Us</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ea
+              quisquam sit eveniet, deleniti consectetur in soluta accusantium
+              eligendi et consequatur ut aliquid, officia quas ab dolore
+              excepturi pariatur quia numquam accusamus. Consequuntur voluptate
+              illo quasi Dignissimos distinctio repellat repudiandae dolorem
+              obcaecati. laborum, quidem dolorum voluptates provident debitis
+              ullam iste fuga minus voluptatum vero.
+            </p>
+            <ul className={styles.socials}>
+              <li>
+                <a href="#">
+                  <Facebook />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Twitter />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Instagram />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <YouTube />
+                </a>
+              </li>
+            </ul>
           </div>
           <div>
-            <p>+1 234 567 8900</p>
-            <p>+1 234 567 8900</p>
+            <h3>Quck Links</h3>
+            <p>About</p>
+            <p>FAQ</p>
+            <p>Privacy Policy</p>
+            <p>Help</p>
+            <p>Terms & Conditions</p>
+            <p>Contact</p>
           </div>
           <div>
-            <p>knowmore@email.com</p>
+            <h3>Contact Us</h3>
+            <div>
+              <p>647 Linda Street Phoenixville, PA 163460, USA</p>
+            </div>
+            <div>
+              <p>+1 234 567 8900</p>
+              <p>+1 234 567 8900</p>
+            </div>
+            <div>
+              <p>knowmore@email.com</p>
+            </div>
           </div>
         </div>
       </footer>
